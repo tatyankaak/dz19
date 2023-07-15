73c586966715a6a9319eecd9524e6c9f31feb452
Татьяна <robsten9086@mail.ru>
2023-07-15 13:28:01 +0300

    задача 19

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 15621;
+string numberStr = Convert.ToString(number);
+if (numberStr[0] == numberStr[4] && numberStr[1] == numberStr[3] )
+{
+    Console.WriteLine($"Число {number} палиндром");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ палиндром");
+}
